8d636f93c1b8d20396e2408d4e1a87bf7d53ee9c
chenyouzeng <409121961@qq.com>
2019-05-14 18:32:49 +0800

    ch04-spring mvc

---
+++ ch04-spring-mvc/src/main/java/com/chen/learn/config/WebInitializer.java
@@ -0,0 +1,17 @@
+package com.chen.learn.config;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class WebInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(MyMvcConfig.class);
+        context.setServletContext(servletContext);
+    }
+}
